cbf32b9869b110dd3379520ab71f0e42567d3845
Michel Ribeiro Correa <michel.ribeiro@inatel.br>
2023-03-22 21:58:45 -0300

    newv

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,25 @@
-import { useState } from "react";
-import './App.css';
 
 export default function App() {
 
-  const [result, setResult] = useState("");
-  const [n1, setN1] = useState("");
-  const [n2, setN2] = useState("");
-  const [erro, setErro] = useState("");
-  const [operation, setOperation] = useState("");
-
-  const handleOperation = (event) => {
-    event.preventDefault();
-    console.log(operation)
-
-    if (!n1 || !n2) {
-      setErro("Digite os dois números")
-      return
-    }
-
-    if (operation === "divisao" && n2 === "0") {
-      setErro("Não é possível dividir por 0")
-      return
-    }
-
-    if (operation === "") {
-      setErro("Selecione uma operação")
-      return
-    }
-
-    switch (operation) {
-      case "soma":
-        setResult(Number(n1) + Number(n2))
-        break;
-      case "subtracao":
-        setResult(Number(n1) - Number(n2))
-        break;
-      case "multiplicacao":
-        setResult(Number(n1) * Number(n2))
-        break;
-      case "divisao":
-        setResult(Number(n1) / Number(n2))
-        break;
-      default:
-        setResult("Selecione uma operação")
-        break;
-    }
-
-    setErro("")
-    setOperation("")
-    setN1(result)
-    setN2("")
-  }
-
-
   return (
-    <div>
-      <h1>Calculadora</h1>
-      <h2>Digite o primeiro numero da operação:</h2>
-      <form onSubmit={(e) => handleOperation(e)}>
-        <input type="number" value={n1} onChange={(e) => setN1(e.target.value)} required />
-        <select onChange={(e) => setOperation(e.target.value)} required value={operation}>
-          <option disabled value={""}>Selecione uma operação</option>
-          <option value="soma">Soma</option>
-          <option value="subtracao">Subtração</option>
-          <option value="multiplicacao">Multiplicação</option>
-          <option value="divisao">Divisão</option>
-        </select>
-        <h2>Digite o segundo número da operação:</h2>
-        <input type="number" value={n2} onChange={(e) => setN2(e.target.value)} required />
+    <main className="min-h-screen">
+      <h1 className="text-black text-3xl text-center py-9">
+        Calculator
+      </h1>
 
-        <button>
-          Calcular
-        </button>
-      </form>
-      {erro !== '' && <h2>Erro: {erro}</h2>}
-      {result !== '' && <h2>Resultado: {result}</h2>}
-    </div>
-
+      <div className="flex flex-col w-80 mx-auto h-[500px] border-2 border-black rounded p-3 shadow-md bg-cyan-900">
+        <div className="h-1/3 bg-cyan-900 w-full">
+          a
+        </div>
+        <div className="h-1 bg-cyan-500 w-full rounded"></div>
+        
+        <div className="h-full bg-cyan-800 w-full">
+          <div className="w-full flex flex-row justify-between pt-4">
+            <button className="w-1/4 h-full bg-cyan-800 text-orange-400 text-2xl">C</button>
+            <button className="w-1/4 h-full bg-cyan-800 text-cyan-500 text-2xl">/</button>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
